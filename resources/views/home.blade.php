e1523154aa8af57a24f18b4d87974c3c66aa4e66
Shijuuten <achmedali@student.ub.ac.id>
2023-12-08 17:11:57 +0700

    Update home page dan style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,142 +1,148 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid p-0" style="margin-top: 80px; border-radius: 25%">
-    <div class="row g-0">
-        <div class="col-md-7">
-            <div id="carouselControl" class="carousel slide h-100" data-bs-ride="carousel">
-                <div class="carousel-inner h-100">
-                    <div class="carousel-item active">
-                        <img src="asset/GHST.jpg" class="d-block w-100 h-100" alt="Slide 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="asset/GW.jpg" class="d-block w-100 h-100" alt="Slide 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="asset/Tekken.jpg" class="d-block w-100 h-100" alt="Slide 3">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselControl" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselControl" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-        <div class="col-md-5 d-flex align-items-center">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Lorem Ipsum</h5>
-                    <p class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <h2 class="section-heading"><strong>Cara Pemesanan</strong></h2>
-        <p class="mb-5">Langkah yang mudah untuk menyewa mobil</p>
-    <div class="row mb-5">
-        <div class="col-lg-4 mb-4 mb-lg-0">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">1. Pilih Device</h5>
-              <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+  <div class="container-fluid p-0 bg-light bg-gradient" style="margin-top: 80px;">
+      <div class="row g-0">
+          <div class="col-md-7">
+              <div id="carouselControl" class="carousel slide h-100" data-bs-ride="carousel">
+                  <div class="carousel-inner h-100">
+                      <div class="carousel-item active">
+                          <img src="asset/GHST.jpg" class="d-block w-100 h-100" alt="Slide 1">
+                      </div>
+                      <div class="carousel-item">
+                          <img src="asset/GW.jpg" class="d-block w-100 h-100" alt="Slide 2">
+                      </div>
+                      <div class="carousel-item">
+                          <img src="asset/Tekken.jpg" class="d-block w-100 h-100" alt="Slide 3">
+                      </div>
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselControl" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselControl" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                  </button>
+              </div>
           </div>
-        </div>
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <div class="card" style="width: 18rem;">
+          <div class="col-md-5 d-flex align-items-center">
+              <div class="card h-100">
+                  <div class="card-body">
+                      <h5 class="card-title">Game Corner</h5>
+                      <p class="card-text">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="site-section">
+      <div class="container"  style="margin-top: 80px;">
+        <h2 class="section-heading"><strong>Cara Pemesanan</strong></h2>
+        <p class="mb-5 mt-2">Langkah yang mudah untuk menyewa mobil</p>
+          <div class="row mb-5">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+              <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 18rem; margin-left: 15%">
+                <div class="card-body">
+                  <h5 class="card-title">1. Pilih Device</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+                  <p class="card-text">tentukan device yang akan dimainkan</p>
+                </div>
+              </div>
+            </div>
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 18rem; margin-left: 15%">
                 <div class="card-body">
                   <h5 class="card-title">2. Isi Form</h5>
                   <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  
+                  <p class="card-text">Isi form pendaftaran sesuai petunjuk</p>
+                    
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 mb-4 mb-lg-0">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">3. Berikan KTM</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      
-                    </div>
-                  </div>
-                </div>
-    </div>                
-    </div>
-    <div class="row">
-        <div class="col-md-6 col-lg-4 mb-4">
-            <div class="card" style="width: 18rem;">
-                <img src="asset/PS.png" class="card-img-top">
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 18rem; margin-left: 15%">
                 <div class="card-body">
-                  <h5 class="card-title">PS5 1</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <h5 class="card-title">3. Berikan KTM</h5>
+                  <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+                  <p class="card-text">Serahkan KTM saat akan menggunakan Device</p>
+                        
                 </div>
               </div>
+            </div>
+          </div>                
         </div>
-        <div class="col-md-6 col-lg-4 mb-4">
-            <div class="card" style="width: 18rem;">
-                <img src="asset/PS2.jpg" class="card-img-top" >
-                <div class="card-body">
-                  <h5 class="card-title">PS5 2</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+      </div>
+      <div class="site-section">
+        <div class="container"  style="margin-top: 80px ;">
+          <h2 class="section-heading"><strong>Daftar Device</strong></h2>
+          <p class="mb-5 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <div class="row ">
+              <div class="col-md-6 col-lg-4 mb-4">
+                  <div class="card" style="width: 18rem; margin-left: 15%">
+                      <img src="asset/PS.png" class="card-img-top">
+                      <div class="card-body">
+                        <h5 class="card-title">PS5 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Reservasi</a>
+                      </div>
+                    </div>
               </div>
-        </div>
-        <div class="col-md-6 col-lg-4 mb-4">
-            <div class="card" style="width: 18rem;">
-                <img src="asset/PCFIX.png" class="card-img-top" >
-                <div class="card-body">
-                  <h5 class="card-title">Konputer</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+              <div class="col-md-6 col-lg-4 mb-4">
+                  <div class="card" style="width: 18rem; margin-left: 15%">
+                      <img src="asset/PS2.jpg" class="card-img-top" >
+                      <div class="card-body">
+                        <h5 class="card-title">PS5 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Reservasi</a>
+                      </div>
+                    </div>
               </div>
+              <div class="col-md-6 col-lg-4 mb-4">
+                  <div class="card" style="width: 18rem; margin-left: 15%">
+                      <img src="asset/PCFIX.png" class="card-img-top" >
+                      <div class="card-body">
+                        <h5 class="card-title">Komputer</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Reservasi</a>
+                      </div>
+                    </div>
+              </div>
+                  <div class="col-md-6 col-lg-4 mb-4">
+                      <div class="card" style="width: 18rem; margin-left: 15%">
+                          <img src="asset/Xbox.jpg" class="card-img-top">
+                          <div class="card-body">
+                            <h5 class="card-title">Xbox 1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Reservasi</a>
+                          </div>
+                        </div>
+                  </div>
+                  <div class="col-md-6 col-lg-4 mb-4 ">
+                      <div class="card" style="width: 18rem; margin-left: 15%">
+                          <img src="asset/Xbox2Fix.jpg" class="card-img-top">
+                          <div class="card-body">
+                            <h5 class="card-title">Xbox 2</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Reservasi</a>
+                          </div>
+                        </div>
+                  </div>
+                  <div class="col-md-6 col-lg-4 mb-4">
+                      <div class="card" style="width: 18rem; margin-left: 15%">
+                          <img src="asset/PUMP.jpeg" class="card-img-top">
+                          <div class="card-body">
+                            <h5 class="card-title">Pump Dance</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Reservasi</a>
+                          </div>
+                        </div>
+                  </div>
+          </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 col-lg-4 mb-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="asset/Xbox.jpg" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Xbox 1</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="asset/Xbox2Fix.jpg" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Xbox 2</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-            </div>
-            <div class="col-md-6 col-lg-4 mb-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="asset/PUMP.jpeg" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Pump Dance</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-            </div>
-        </div> 
-    </div>
-</div>
+      </div>
+  </div>
 
 <style>
     #carouselControl {
---
+++ resources/views/reservasi.blade.php
@@ -0,0 +1,47 @@
+<div class="hero" style="background-image: url('{{ asset('BG1.png') }}')">
+<div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-lg-10">
+              <div class="row mb-5">
+                <div class="col-lg-7 intro">
+                  <h1><strong>Sewa Mobil</strong> Dengan Satu Sentuhan.</h1>
+                </div>
+              </div>
+
+              <form class="trip-form" >
+                <div class="row align-items-center">
+                  <div class="mb-3 mb-md-0 col-md-4">
+                    <select name="category_id" id="category_id" class="custom-select form-control">
+                      <option value="">Pilih Device</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+
+                    </select>
+                  </div>
+                  <div class="mb-3 mb-md-0 col-md-4">
+                    <select name="penumpang" id="penumpang" class="custom-select form-control">
+                      <option value="">Waktu</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                  </div>
+
+                  <div class="mb-3 mb-md-0 col-md-4">
+                    <input
+                      type="submit"
+                      value="Check"
+                      class="btn btn-primary btn-block py-3"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
